f9eea9370fe62e2cab9179fa1032415b7aa425e4
Eden Mesfun <emesfun@gmail.com>
2018-01-23 20:31:09 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(numbers)
-  square_array.each do |element|
+  square_array.each do |numbers|
     # print element**2
     # square_array << element**2
     # print square_array(element)
