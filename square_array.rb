7518d098560d543e739a62d7ae2e975b28a51605
Eden Mesfun <emesfun@gmail.com>
2018-01-23 20:48:31 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,15 @@
-def square_array(element)
-  element.each
-  element = element * 2;
+# def square_array(element)
+#   element.each
+#   element = element * 2;
+# 
+# end
 
+
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+my_arr = [1, 2]
+p square_array(my_arr) #=> [1, 4]
